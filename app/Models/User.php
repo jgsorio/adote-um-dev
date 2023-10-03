50792505918ffef4205b3c7e3df8c927907ac493
Jose Guilherme Sorio <joseguilhermesorio@gmail.com>
2023-10-02 22:48:59 -0300

    feat: create all relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -42,4 +44,29 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function interest(): HasOne
+    {
+        return $this->hasOne(Interest::class);
+    }
+
+    public function receiveActions(): HasMany
+    {
+        return $this->hasMany(Action::class, 'from_user_id', 'id');
+    }
+
+    public function sentActions(): HasMany
+    {
+        return $this->hasMany(Action::class, 'to_user_id', 'id');
+    }
+
+    public function receiveMessages(): HasMany
+    {
+        return $this->hasMany(Message::class, 'from_user_id', 'id');
+    }
+
+    public function sentMessages(): HasMany
+    {
+        return $this->hasMany(Message::class, 'to_user_id', 'id');
+    }
 }
